eeb8d8d80cee79c6f3d613bb38126a95e3afd3a0
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 11:51:01 -0300

    ajuste controller

--- PrototipoERP/Controllers/UsuariosController.cs
+++ PrototipoERP/Controllers/UsuariosController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using PrototipoERP.Entidades;
 using Microsoft.AspNetCore.Mvc;
-using PrototipoERP.Configuration;
-using PrototipoERP.Entidades;
+using Microsoft.AspNetCore.Authorization;
+using PrototipoERP.Configuracoes.Criptografia;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
