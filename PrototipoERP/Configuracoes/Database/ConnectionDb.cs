646658676c73bc29ed56fa3ad2a7db3773a97a5f
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 12:25:42 -0300

    change password db

--- PrototipoERP/Configuracoes/Database/ConnectionDb.cs
+++ PrototipoERP/Configuracoes/Database/ConnectionDb.cs
@@ -2,6 +2,6 @@
 {
     public static class ConnectionDb
     {
-        public const string ConnectionString = "Server=localhost;Database=mariadbteste;User=root;Password=admin;";
+        public const string ConnectionString = "Server=localhost;Database=mariadbteste;User=root;Password=root;";
     }
 }
