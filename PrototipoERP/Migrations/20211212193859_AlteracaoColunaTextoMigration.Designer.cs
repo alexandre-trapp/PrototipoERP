52d51eb213a2480fece3ce4cda4eb3dc7e096135
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 16:39:18 -0300

    migration coluna texto

--- PrototipoERP/Migrations/20211212193859_AlteracaoColunaTextoMigration.Designer.cs
+++ PrototipoERP/Migrations/20211212193859_AlteracaoColunaTextoMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrototipoERP.Infraestrutura.Database;
 
 namespace PrototipoERP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211212193859_AlteracaoColunaTextoMigration")]
+    partial class AlteracaoColunaTextoMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
