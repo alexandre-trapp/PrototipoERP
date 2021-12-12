e7e76e2419bb7269dd6c458ed1a67d9b2db89fc1
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 16:26:24 -0300

    ajuste id not ignore

--- PrototipoERP/Entidades/Entity.cs
+++ PrototipoERP/Entidades/Entity.cs
@@ -4,7 +4,6 @@ namespace PrototipoERP.Entidades
 {
     public class Entity
     {
-        [System.Text.Json.Serialization.JsonIgnore]
         [JsonProperty(Required = Required.Default)]
         public long Id { get; set; }
     }
