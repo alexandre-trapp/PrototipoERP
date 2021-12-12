f1e2c4df59bfb46ad9151565ddedfa7e06388521
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 10:48:24 -0300

    add OpenApiSecurityScheme authorization bearer on swagger

--- PrototipoERP/Program.cs
+++ PrototipoERP/Program.cs
@@ -2,13 +2,29 @@ using System.Text;
 using PrototipoERP.Configuration;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    })
+);
 
 builder.Services.AddCors();
 
