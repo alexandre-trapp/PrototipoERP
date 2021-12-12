66ff3dfedf9757a0bb354bad290d7f34cebf1b1e
Alexandre <alexandre.trapp@hotmail.com>
2021-12-12 16:11:15 -0300

    ajuste id entity jsonignore

--- PrototipoERP/Entidades/Entity.cs
+++ PrototipoERP/Entidades/Entity.cs
@@ -1,7 +1,11 @@
-﻿namespace PrototipoERP.Entidades
+﻿using Newtonsoft.Json;
+
+namespace PrototipoERP.Entidades
 {
     public class Entity
     {
+        [System.Text.Json.Serialization.JsonIgnore]
+        [JsonProperty(Required = Required.Default)]
         public long Id { get; set; }
     }
 }
