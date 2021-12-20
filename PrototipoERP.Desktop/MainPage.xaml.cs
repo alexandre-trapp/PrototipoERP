816b4789672df76784500307685ae21453cd01e0
Alexandre <alexandre.trapp@hotmail.com>
2021-12-20 13:53:18 -0300

    add desktop maui app PrototipoERP

---
+++ PrototipoERP.Desktop/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace PrototipoERP.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLembretesClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Lembrete count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
